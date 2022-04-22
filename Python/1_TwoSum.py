912c973a65b697048c7428b00eb5275286b27585
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-04-22 21:43:13 +0100

    Add Python solution for problem 1: "Two Sum"

---
+++ Python/1_TwoSum.py
@@ -0,0 +1,19 @@
+"""
+The first approach to this problem would be to iterate over the array 
+and check all possible pairs of numbers, but that would be 
+a O(n²) solution. A less naive approach would be to iterate over
+the array and adding the items to an hash table. 
+Whenever adding a item, you would check if there is the complement needed
+to reach the target in the hash table, giving us a O(n) solution.
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {};
+        for i in range(len(nums)):
+            n = target - nums[i]
+            if n not in hash_table:
+                hash_table[nums[i]] = i;
+            else:
+                return [hash_table[n], i]
+
